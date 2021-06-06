52c8701ce7168ac503dfd724f410cc1dbbcd54e6
jerred-shifflett <jerredshifflett@gmail.com>
2021-06-06 14:19:28 -0700

    lint error stiil found

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import example
 class TestCase(unittest.TestCase):
 
 	def test_add_1(self):
-		self.assertEqual(example.add(1,2),3)
+		self.assertEqual(example.add(1, 2), 3)
 
 	def test_subtract_1(self):
-		self.assertEqual(example.subtract(1,1),0)
+		self.assertEqual(example.subtract(1, 1), 0)
 
 if __name__ == '__main__':
 	unittest.main()
